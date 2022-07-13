56fe9ec6631f9ea5f15ff124a078aaaf17f6adf9
martin0123478 <martin_agui97@outlook.es>
2022-07-13 18:53:09 -0500

    viendo visualmente las tareas

--- src/app/todos/todo-item/todo-item.component.ts
+++ src/app/todos/todo-item/todo-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Todo } from '../models/todo.model';
 
 @Component({
   selector: 'app-todo-item',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-item.component.css']
 })
 export class TodoItemComponent implements OnInit {
-
-  constructor() { }
+  @Input()
+  todo!: Todo; 
+  constructor() { 
+    
+  }
 
   ngOnInit(): void {
+  
   }
 
 }
